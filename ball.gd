402cad73003333ff849319b5d8cc09935c10ef26
Myron Lioz <liozmyron@gmail.com>
2025-10-16 13:21:43 -0700

    fix issues with ball movement and collision

--- ball.gd
+++ ball.gd
@@ -1,11 +1,12 @@
-extends RigidBody2D
+extends Area2D
 
 @onready var screen_size: Vector2 = get_viewport().size
 
 @export var speed: int = 200
 @export var color: Color = Color.WHITE
 
-var velocity: Vector2 = Vector2(-speed, 0)
+var initial_velocity: Vector2 = Vector2(-speed, speed)
+var current_velocity: Vector2 = initial_velocity
 var reset_pos: bool = false
 
 signal ball_position(pos: Vector2)
@@ -15,21 +16,23 @@ func _draw() -> void:
 	draw_rect(Rect2(-8, -8, 16, 16), color, true)
 	
 func _process(delta: float) -> void:
-	pass
-	
-
-func _physics_process(delta: float) -> void:
-	var info = move_and_collide(velocity * delta)
-	
-	if info:
-		velocity = velocity.bounce(info.get_normal())
-	
-	ball_position.emit(position)
-
-func _integrate_forces(_state: PhysicsDirectBodyState2D) -> void:
-	if reset_pos:
+	if reset_pos == true:
 		position = screen_size / 2
+		current_velocity = initial_velocity
 		reset_pos = false
+	else:
+		position += current_velocity * delta
+	
+		if position.y < 0 or position.y > screen_size.y:
+			current_velocity.y = -current_velocity.y
+		
+		ball_position.emit(position)
+
+func _on_area_entered(area: Area2D) -> void:
+	if area.name == "Player":
+		current_velocity.x = abs(current_velocity.x)
+	elif area.name == "AI":
+		current_velocity.x = -abs(current_velocity.x)
 
 func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
 	ball_exited_screen.emit()
