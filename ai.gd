d1980bcfeb487330068e7eb85386b17a57424825
Myron Lioz <liozmyron@gmail.com>
2025-10-17 15:52:42 -0700

    ai moves to predicted y coordinate on ai side wall

--- ai.gd
+++ ai.gd
@@ -1,12 +1,17 @@
 extends Area2D
 
 @onready var screen_size: Vector2 = get_viewport().size
+@onready var new_position: Vector2
 
 @export var speed: float = 500
 @export var paddle_size: Vector2 = Vector2(16, 128)
 @export var color: Color = Color.RED
 
-var ball_position: Vector2 = screen_size / 2
+var direction = 0
+
+func _ready() -> void:
+	center_paddle_position()
+	new_position = position
 
 func _draw():
 	draw_rect(Rect2(paddle_size.x / 2 * -1, paddle_size.y / 2 * -1, paddle_size.x, paddle_size.y), color, true)
@@ -16,10 +21,11 @@ func center_paddle_position() -> void:
 	position.y = screen_size.y / 2
 
 func _process(delta: float) -> void:
-	position.y += position.direction_to(ball_position).normalized().y * speed * delta
+	position.y = move_toward(position.y, new_position.y, speed * delta)
 	
 	# Prevents the paddle from going off the screen
 	position.y = clampf(position.y, paddle_size.y / 2, screen_size.y - paddle_size.y / 2)
 
-func _on_ball_ball_position(pos: Vector2) -> void:
-	ball_position = pos
+func _on_ball_predicted_ball_bounce(y: int) -> void:
+	new_position.y = y
+	direction = (new_position.y - position.y)/abs(new_position.y - position.y)
--- ball.gd
+++ ball.gd
@@ -13,7 +13,7 @@ var initial_velocity: Vector2 = Vector2(-speed, speed)
 var current_velocity: Vector2 = initial_velocity
 var reset_pos: bool = false
 
-signal ball_position(pos: Vector2)
+signal predicted_ball_bounce(y: int)
 signal ball_exited_screen
 
 func _draw() -> void:
@@ -21,23 +21,47 @@ func _draw() -> void:
 	
 func _process(delta: float) -> void:
 	position += current_velocity * delta
+	
 	if position.y < 0 or position.y > screen_size.y:
 		current_velocity.y = -current_velocity.y
 		high_bounce_sound.play()
-		
-	ball_position.emit(position)
+		check_and_emit_ball_bounce_position(position, current_velocity)
 
 func _on_area_entered(area: Area2D) -> void:
 	if area.name == "Player":
 		current_velocity.x = abs(current_velocity.x - speed_increase)
 		low_bounce_sound.play()
+		check_and_emit_ball_bounce_position(position, current_velocity)
+		
 	elif area.name == "AI":
 		current_velocity.x = -abs(current_velocity.x + speed_increase)
 		low_bounce_sound.play()
-
+	
 func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
 	ball_exited_screen.emit()
 
 func center_ball() -> void:
 	position = screen_size / 2
 	current_velocity = initial_velocity
+
+func check_and_emit_ball_bounce_position(ball_pos: Vector2, ball_vel: Vector2) -> void:
+	if current_velocity.x > 0:
+			var ball_y_bounce_predictions = get_preicted_ball_bounce(ball_pos, ball_vel)
+			
+			# AI shouldn't move if the ball's bounce y coordinate is not on screen
+			if ball_y_bounce_predictions > 0:
+				predicted_ball_bounce.emit(ball_y_bounce_predictions)
+
+func get_preicted_ball_bounce(pos: Vector2, vel: Vector2) -> int:
+	 #Use the parametric equation for the ball's path to find what y value it will bounce on the AI's wall
+	 #pos = (posx, posy)
+	 #vel = (velx, vely)
+	 #x(t) = posx + velx*t
+	 #y(t) = posy + vely*t
+	 #t = (x - pox)/velx
+	 #y = posy + vely/velx*(x-posx)
+	
+	var ai_paddle_x_pos = screen_size.x - 25
+	var ball_y_bounce = pos.y + vel.y/vel.x * (ai_paddle_x_pos - pos.x)
+	
+	return ball_y_bounce
